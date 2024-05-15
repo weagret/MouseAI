3eb2244bfb2497b0e9bc6641faf0eb3fe51c2547
weagret <uelmurodov@internet.ru>
2024-05-15 23:27:47 +0300

    feat: add hand_tracking.py

---
+++ hand_tracking.py
@@ -0,0 +1,37 @@
+import cv2
+import mediapipe as mp
+
+class handAndGestureDetector:
+    def __init__(
+            self,
+            staticMode=False,
+            maxHands=1,
+            detectionConfidence=0.6,
+            trackConfidence=0.6
+        ):
+
+        self.mode = staticMode
+        self.maxHands = maxHands
+        self.detectionConfidence = detectionConfidence
+        self.trackConfidence = trackConfidence
+
+        self.mpHands = mp.solution
+
+        self.hand = self.mpHands.Hands(
+            static_image_mode=self.mode,
+            model_complexity=0,
+            max_num_hands=self.maxHands,
+            min_detection_confidence=self.detectionConfidence,
+            min_tracking_confidence=self.trackConfidence
+        )
+
+    def start(self):
+        video = cv2.VideoCapture(0)
+
+
+def main():
+    detector = handAndGestureDetector()
+    detector.start()
+
+if __name__ == "__name__":
+    main()
