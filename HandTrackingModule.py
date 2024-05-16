403d8a6a77d9b85b0450a5e18b868cb205cb7f7a
weagret <uelmurodov@internet.ru>
2024-05-16 08:39:30 +0300

    feat: add hand tracking module

---
+++ HandTrackingModule.py
@@ -0,0 +1,57 @@
+import cv2
+import mediapipe as mp
+
+
+
+class handDetector:
+    def __init__(
+            self,
+            mode=False,
+            maxHands=1,
+            modelComplexity=0,
+            detectionConfidence=0.6,
+            trackConfidence=0.6
+        ):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.modelComplexity = modelComplexity
+        self.detectionCon = detectionConfidence
+        self.trackCon = trackConfidence
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(
+            self.mode,
+            self.maxHands,
+            self.modelComplexity,
+            self.detectionCon,
+            self.trackCon
+        )
+        self.tipIds = [4, 8, 12, 16, 20]
+
+    def findHands(self, img):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        imgRGB = cv2.flip(img, 1)
+        self.results = self.hands.process(imgRGB)
+        return img
+
+    def findPosition(self, img, handNo=0):
+        self.lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, _ = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                self.lmList.append([id, cx, cy])
+        return self.lmList
+
+
+    def fingersUp(self):
+        fingers = []
+
+        fingers.append(self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0] - 1][1])
+        for id in range(1, 5):
+            fingers.append(self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2])
+        return fingers
+
+
+
